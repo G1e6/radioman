73d8db1ba26b1a51318e5b01ea2307db8b027107
G1e6 <gleb-potapov@rambler.ru>
2022-07-03 23:19:45 +0500

    new branch2

--- src/main/java/ru/ru/netology/javaradio/service/Radio.java
+++ src/main/java/ru/ru/netology/javaradio/service/Radio.java
@@ -3,35 +3,49 @@ package ru.ru.netology.javaradio.service;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
 
-    public void next(){
-        if (currentStation < 9){
-            currentStation = currentStation+1;
-        }else{
+    public Radio() {
+        maxStation = 9;
+    }
+
+    public Radio(int stationCount) {
+        maxStation = stationCount - 1;
+    }
+
+    public void next() {
+        if (currentStation < maxStation) {
+            currentStation = currentStation + 1;
+        } else {
             currentStation = 0;
         }
     }
 
-    public void prev(){
-        if (currentStation > 0){
-            currentStation = currentStation-1;
-        }else{
-            currentStation = 9;
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = maxStation;
         }
     }
 
     public void increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < 100) {
             currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = 100;
         }
+
     }
 
-    public void reduceVolume(){
-        if (currentVolume > 0){
+    public void reduceVolume() {
+        if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = 0;
         }
-    }
 
+    }
 
 
     public int getCurrentStation() {
@@ -39,10 +53,10 @@ public class Radio {
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0){
+        if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9){
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
@@ -53,11 +67,11 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0 ){
+        if (currentVolume < 0) {
             return;
         }
-        if (currentVolume > 10){
-            return;
+        if (currentVolume > 100) {
+            currentVolume = 100;
         }
         this.currentVolume = currentVolume;
     }
